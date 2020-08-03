24ab3c9b993dffac891fe5ba05c0dfb446612086
gtongtz <69032120+gtongtz@users.noreply.github.com>
2020-08-03 13:46:21 +0800

    Add files via upload

---
+++ 10-35.py
@@ -0,0 +1,16 @@
+
+import matplotlib.pyplot as plt
+import xlrd
+xl = xlrd.open_workbook('/Users/getong/Desktop/10-35.xlsx')
+table = xl.sheets()[0]
+row = table.row_values(1)
+col = table.col_values(0)
+accx = table.col_values(5,0)
+accy = table.col_values(9,0)
+accz = table.col_values(19,0)
+plt.title('Result Analysis')
+plt.plot(accx,color="green",label="accx")
+plt.plot(accy,color="yellow",label="accy")
+plt.plot(accz,color="red",label="accz")
+plt.legend()
+plt.show()
